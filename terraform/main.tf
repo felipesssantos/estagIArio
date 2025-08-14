b6c20a2cc2e362b4b05d9c0a768e0f524067d499
Felipe Santos <felipesssantos.ti@gmail.com>
2025-08-14 09:52:47 -0300

    Fix main.tf - Gerar token de autenticação

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,9 +64,14 @@ module "eks" {
   }
 }
 
-# 3. Configura o provider do Kubernetes para se conectar ao cluster
+# 3. Usa uma fonte de dados para gerar um token de autenticação para o cluster
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+# 4. Configura o provider do Kubernetes para se conectar ao cluster
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = module.eks.cluster_kubeconfig_user_token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
